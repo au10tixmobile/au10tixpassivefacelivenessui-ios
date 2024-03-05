ab505b52c4f279513dc2f37f99a835e488d833d0
au10tixmobile <94300864+au10tixmobile@users.noreply.github.com>
2024-03-05 19:01:28 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,39 +14,9 @@ import Foundation
 let package = Package(
     name: "au10tixpassivefacelivenessui-ios",
     products: [ 
-        .library(name: "Au10tixCore", targets: ["Au10tixCore"]),
-        .library(name: "Au10tixSourceManager", targets: ["Au10tixSourceManager"]),
-        .library(name: "Au10tixDetectorManager", targets: ["Au10tixDetectorManager"]),
-        .library(name: "Au10tixPassiveFaceLivenessKit", targets: ["Au10tixPassiveFaceLivenessKit"]),
-        .library(name: "Au10tixBaseUI", targets: ["Au10tixBaseUI"]),
         .library(name: "Au10tixPassiveFaceLivenessUI", targets: ["Au10tixPassiveFaceLivenessUI"])
     ],
     targets: [
-        .binaryTarget(
-          name: "Au10tixCore",
-          url: "https://github.com/au10tixmobile/Au10tixCore/archive/refs/tags/4.0.0.zip",
-          checksum: "ed5691a5d3bf0e519daa6c0e814c9900d77bd6db00aac68b1931afc87d6683f6"
-        ),
-        .binaryTarget(
-          name: "Au10tixSourceManager",
-          url: "https://github.com/au10tixmobile/Au10tixSourceManager/archive/refs/tags/4.0.0.zip",
-          checksum: "8bc55b8fe260a75fcd1a191e2c0f02faf1c10b805c4cbf7c00ea8eb74c66ec1f"
-        ),
-        .binaryTarget(
-          name: "Au10tixDetectorManager",
-          url: "https://github.com/au10tixmobile/Au10tixDetectorManager/archive/refs/tags/4.0.0.zip",
-          checksum: "58741a60c387dd59de4d184db761d007d0ac88a9989b6dac66e1e6627f9d93c7"
-        ),
-        .binaryTarget(
-          name: "Au10tixPassiveFaceLivenessKit",
-          url: "https://github.com/au10tixmobile/Au10tixPassiveFaceLivenessKit/archive/refs/tags/4.0.0.zip",
-          checksum: "32ba28df7109a95f3ee0e5cf694ef0f5a74303185786cc0d4e9c64088dbe6fe2"
-        ),
-        .binaryTarget(
-          name: "Au10tixBaseUI",
-          url: "https://github.com/au10tixmobile/Au10tixBaseUI/archive/refs/tags/4.0.0.zip",
-          checksum: "42eec055956b9973b0daef9548816b7a20f11baf33518010f73205d683e32fa4"
-        ),
         .binaryTarget(
           name: "Au10tixPassiveFaceLivenessUI",
           url: "https://github.com/au10tixmobile/Au10tixPassiveFaceLivenessUI/archive/refs/tags/4.0.0.zip",
