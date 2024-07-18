8c672278df1c0b2298edce309cd9210335e17bf9
au10tixmobile-pipeline <au10tixmobile@au10tix.com>
2024-07-18 08:12:39 +0000

    Updating Package.swift to version 4.2.1 automatically from ios pipeline

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
           name: "Au10tixPassiveFaceLivenessUI",
-          url: "https://github.com/au10tixmobile/Au10tixPassiveFaceLivenessUI/archive/refs/tags/4.2.0.zip",
-          checksum: "f0d28195f96b167e1acf728cc016b1189872e6334a433ace693b74099b893608"
+          url: "https://github.com/au10tixmobile/Au10tixPassiveFaceLivenessUI/archive/refs/tags/4.2.1.zip",
+          checksum: "65b1165269b6a4cea9bdf5f15a05515f50816137ed866d56316cbafc34edca65"
         )
     ]
 )
